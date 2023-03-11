9df027fe92b445e361b2cc38ab606b65f535af1f
Sagynaliev Taalaibek <sagynalievv.t@gmail.com>
2023-03-11 11:35:31 +0600

    lab5 Controller (Dish, Menu, Order, Restoramt) and For each Controller created view

---
+++ src/Controllers/CourierController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EatAndDrink.Controllers
+{
+    public class CourierController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
